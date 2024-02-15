31f1de11d76f61d01c3c2967e00af1e56ca4285a
Yash Verma <yash1997verma@gmail.com>
2024-02-15 12:38:25 +0530

    app init

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Main from './components/Main/Main'
+function App() {
+
+  return (
+   <div className='flex items-center justify-center bg-[#e2e1dd] h-[100vh]'>
+      <Main />
+   </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Main/Main.jsx
@@ -0,0 +1,8 @@
+
+function Main() {
+  return (
+    <div className=" bg-white rounded-md shadow-md h-[90vh] w-[90vw]">Main</div>
+  )
+}
+
+export default Main
